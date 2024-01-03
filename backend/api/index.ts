b4903afb94bacfff7da2e3060a4dfcdc7be55214
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-03 03:47:03 +0100

    finished

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -2,27 +2,51 @@ import * as config from '../config';
 import express from 'express';
 import cors from 'cors';
 import * as tools from '../tools';
+import axios from 'axios';
+
+interface IEmpHireItem {
+	fullName: string;
+	hireDate: string;
+}
 
 const app = express();
 app.use(cors());
 const port = 5112;
 
+// serve OBJECT
 app.get('/', (req, res) => {
 	res.json({
 		appName: config.getAppName()
 	});
 });
 
-// string
+// serve STRING
 app.get('/node-version', (req, res) => {
 	res.json(process.version);
 });
 
-// number
+// serve NUMBER
 app.get('/number-of-files', (req, res) => {
 	const baseDirectory = process.cwd();
 	const files = tools.getAllFilesInDirectory(baseDirectory);
 	res.json(files.length);
+});
+
+// serve ARRAY
+app.get('/employee-hire-dates', (req, res) => {
+	(async () => {
+		const response = await axios.get('https://edwardtanguay.vercel.app/share/employees.json');
+		const empHireItems: IEmpHireItem[] = [];
+		const employees = response.data;
+		for (const employee of employees) {
+			empHireItems.push({
+				fullName: `${employee.firstName} ${employee.lastName}`,
+				hireDate: employee.hireDate.substring(0,10)
+			});
+		}
+		console.log(empHireItems.length);
+		res.json(empHireItems);
+	})();
 });
 
 
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -3,10 +3,16 @@ import { useEffect, useState } from "react";
 
 const backendUrl = "http://localhost:5112";
 
+interface IEmpHireItem {
+	fullName: string;
+	hireDate: string;
+}
+
 export const PageWelcome = () => {
 	const [appName, setAppName] = useState("");
 	const [nodeVersion, setNodeVersion] = useState("");
 	const [numberOfFiles, setNumberOfFiles] = useState(0);
+	const [empHireItems, setEmpHireItems] = useState<IEmpHireItem[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -30,11 +36,28 @@ export const PageWelcome = () => {
 		})();
 	}, []);
 
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(
+				`${backendUrl}/employee-hire-dates`
+			);
+			setEmpHireItems(response.data);
+		})();
+	}, []);
+
 	return (
 		<>
 			<p>APP NAME: {appName}</p>
 			<p>NODE VERSION: {nodeVersion}</p>
-			<p>NUMBER OF FILES: {numberOfFiles}</p>
+			<p>NUMBER OF FILES IN WEBSITE DIRECTORY: {numberOfFiles}</p>
+			<p>
+				EMPLOYEE HIRE DATES:
+				<ul className="list-disc ml-6">
+					{empHireItems.map((empHireItem, index) => {
+						return <li key={index}>{empHireItem.fullName} - {empHireItem.hireDate}</li>;
+					})}
+				</ul>
+			</p>
 		</>
 	);
 };
